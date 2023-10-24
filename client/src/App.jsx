14e62e3104a64567ae5705588624c4bc10465af5
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-24 21:22:44 +1100

    set up app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from "react";
-import "./App.css";
+import { Outlet } from "react-router-dom";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <Header />
+      </header>
+      <Outlet location={location} key={location.pathname} />
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
