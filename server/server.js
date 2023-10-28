7736a908b3a090afb1e1ce655fc159336f9f07cc
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-28 21:32:05 +1100

    added initial server.js

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
+const { authMiddleware } = require("./util/auth");
+const mongoose = require("mongoose");
+
+const { typeDefs, resolvers } = require("./schemas");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+});
+
+const db = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/docswap"
+);
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
