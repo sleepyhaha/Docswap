2405ec57a26bd49b639f37c7bc216b273720e333
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-30 23:46:07 +1100

    change server to include context

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => {
+    return authMiddleware({ req });
+  },
 });
 
 const startApolloServer = async () => {
