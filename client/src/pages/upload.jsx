516c5e202fbbc11eaf579716c0fdc43fb38478a3
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-10-30 20:32:18 +1100

    started submit handler

--- client/src/pages/upload.jsx
+++ client/src/pages/upload.jsx
@@ -1,16 +1,38 @@
 import { Widget } from "@uploadcare/react-widget";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { UPLOAD } from "../util/mutations";
 
 export default function Upload() {
+  const [upload] = useMutation(UPLOAD);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await upload({
+      variables: {
+        title: formState.title,
+        description: formState.description,
+        price: formState.price,
+        preview: formState.preview.response.uuid,
+        location: formState.location.response.uuid,
+      },
+    });
+
+    return mutationResponse;
+  };
+
   return (
     <div className="h-2/3">
       <section className="flex justify-center w-full">
         <h1>Upload</h1>
       </section>
-      <div className="flex flex-col justify-center items-center relative left-40">
+      <div className="flex flex-col justify-end items-center relative left-40">
         <form className="w-1/2 flex mt-56 items-center">
           <p className="mx-7 text-2xl">Title: </p>
           <input
             type="string"
+            name="title"
+            id="title"
             placeholder={"Enter your title here"}
             className="ml-8 py-1 px-9 text-center"
           />
@@ -19,6 +41,8 @@ export default function Upload() {
           <p className="mx-7 text-2xl">Description:</p>
           <input
             type="string"
+            name="description"
+            id="description"
             placeholder={"Enter your description here"}
             className="ml-10 py-1 px-9 text-center"
           />
@@ -27,18 +51,29 @@ export default function Upload() {
           <p className="mr-12 text-2xl">Price:</p>
           <input
             type="integer"
+            name="price"
+            id="price"
             placeholder={"Enter your price here"}
             className="py-1 px-9 ml-1.5 text-center"
           />
         </form>
         <form>
-          <p> Preview: </p>
-          <Widget publicKey="d8ef5a21f4ceb10dd109" id="file" />
+          <p className="float-left"> Preview: </p>
+          <Widget
+            publicKey="d8ef5a21f4ceb10dd109"
+            name="preview"
+            id="preview"
+          />
         </form>
         <form>
           <label htmlFor="file"> Document: </label>
-          <Widget publicKey="d8ef5a21f4ceb10dd109" id="file" />
+          <Widget
+            publicKey="d8ef5a21f4ceb10dd109"
+            name="location"
+            id="location"
+          />
         </form>
+        <button onSubmit={handleSubmit}>Submit</button>
       </div>
     </div>
   );
