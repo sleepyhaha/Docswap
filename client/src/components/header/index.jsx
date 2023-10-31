2d7b0ea9ae3e829d31fc92e75449694c0373477e
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-11-01 00:40:52 +1100

    set header to change buttons on log in

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -1,15 +1,40 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../../util/UserContext";
+import { signOut, signedIn } from "../../util/auth";
 
 export default function Header() {
+  const user = useContext(UserContext);
   return (
     <nav className=" bg-indigo-950 p-5">
-      <h1>DocSwap</h1>
-      <Link to="/signup" className="relative float-right bottom-12">
-        <button>Sign Up</button>
+      <Link to="/">
+        <h1>DocSwap</h1>
       </Link>
-      <Link to="/signin" className="relative float-right bottom-12 right-5">
-        <button>Sign In</button>
-      </Link>
+      {signedIn() ? (
+        <>
+          <Link
+            to="/profile"
+            className="relative float-right bottom-12 right-5"
+          >
+            Profile
+          </Link>
+          <button
+            onClick={signOut}
+            className="relative float-right bottom-12 right-5"
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <Link to="/signup" className="relative float-right bottom-12">
+            <button>Sign Up</button>
+          </Link>
+          <Link to="/signin" className="relative float-right bottom-12 right-5">
+            <button>Sign In</button>
+          </Link>
+        </>
+      )}
     </nav>
   );
 }
