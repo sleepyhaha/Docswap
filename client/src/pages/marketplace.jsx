d4f7b9aa5883edfbf5f74b95603945a65b019378
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-11-02 17:16:18 +1100

    added marketplace view and animations

--- client/src/pages/marketplace.jsx
+++ client/src/pages/marketplace.jsx
@@ -1,13 +1,32 @@
 import { Link } from "react-router-dom";
 
+import { motion } from "framer-motion";
+import { animation, childAnimation } from "../util/animation";
+
 export default function Marketplace() {
   return (
-    <div className="h-3/4 w-full flex flex-col items-center">
-      <section className="flex justify-center w-full mb-16">
+    <motion.div
+      variants={animation}
+      initial="initial"
+      animate="animate"
+      className="h-full w-full flex flex-col items-center"
+    >
+      <motion.section
+        variants={childAnimation}
+        className="flex justify-center w-full mb-16"
+      >
         <h1>Marketplace</h1>
-      </section>
-      <div className="h-full columns-3 w-4/5 flex">
-        <section className="w-72 h-96 bg-stone-300">
+      </motion.section>
+      <motion.div
+        variants={animation}
+        initial="initial"
+        animate="animate"
+        className="h-full columns-3 w-4/5 gap-24"
+      >
+        <motion.section
+          variants={childAnimation}
+          className="w-72 h-96 bg-stone-300 "
+        >
           <title>Seller</title>
           <div>
             <img src="/" alt="seller profile picture" />
@@ -15,8 +34,70 @@ export default function Marketplace() {
           <Link to="/profile">
             <button>View Profile</button>
           </Link>
-        </section>
-      </div>
-    </div>
+        </motion.section>
+        <motion.section
+          variants={childAnimation}
+          className="w-72 h-96 bg-stone-300 mt-5"
+        >
+          <title>Seller</title>
+          <div>
+            <img src="/" alt="seller profile picture" />
+          </div>
+          <Link to="/profile">
+            <button>View Profile</button>
+          </Link>
+        </motion.section>
+        <motion.section
+          variants={childAnimation}
+          className="w-72 h-96 bg-stone-300 mt-5"
+        >
+          <title>Seller</title>
+          <div>
+            <img src="/" alt="seller profile picture" />
+          </div>
+          <Link to="/profile">
+            <button>View Profile</button>
+          </Link>
+        </motion.section>
+        <motion.section
+          variants={childAnimation}
+          className="w-72 h-96 bg-stone-300 mt-5"
+        >
+          <title>Seller</title>
+          <div>
+            <img src="/" alt="seller profile picture" />
+          </div>
+          <Link to="/profile">
+            <button>View Profile</button>
+          </Link>
+        </motion.section>
+
+        <motion.section
+          variants={childAnimation}
+          className="w-72 h-96 bg-stone-300 mt-5"
+        >
+          <title>Seller</title>
+          <div>
+            <img src="/" alt="seller profile picture" />
+          </div>
+          <Link to="/profile">
+            <button>View Profile</button>
+          </Link>
+        </motion.section>
+
+        <motion.section
+          variants={childAnimation}
+          className="w-72 h-96 bg-stone-300 mt-5"
+        >
+          <title>Seller</title>
+          <div>
+            <img src="/" alt="seller profile picture" />
+          </div>
+          <Link to="/profile">
+            <button>View Profile</button>
+          </Link>
+        </motion.section>
+      </motion.div>
+    </motion.div>
   );
 }
