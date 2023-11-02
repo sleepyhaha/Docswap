c4dcfa3516d65992c239f9b190d9abe764de9508
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-11-02 17:13:55 +1100

    styled document view

--- client/src/pages/document.jsx
+++ client/src/pages/document.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Document() {
   return (
-    <div className="h-2/3">
-      <section className="flex justify-center w-full">
+    <div className="h-full flex flex-row flex-wrap w-full m-auto ">
+      <section className="w-full relativeW">
         <h1>Document title</h1>
       </section>
-      <div className="mt-28 columns-2 h-full mx-10">
-        <section className="h-full mx-5">
+      <div className="mt-28 w-2/3 h-full m-auto">
+        <section className="h-1/3 w-1/3 bg-slate-400 m-auto ">
           <img src="*" alt="document title" />
         </section>
         <aside className="h-full mx-5">
@@ -17,7 +17,9 @@ export default function Document() {
             laudantium qui deleniti voluptate illo. Itaque, temporibus. Nulla
             quidem atque quibusdam accusamus at.
           </p>
-          <p>price here</p>
+          <br />
+          <p>$$$$</p>
+          <br />
           <Link to="/purchases">
             <button>Purchase</button>
           </Link>
