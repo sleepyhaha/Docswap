ff39b8f879368f72c3838f58af6642fcb359c582
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-11-02 17:11:37 +1100

    conditional rendering for header

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -4,26 +4,29 @@ import { signOut, signedIn } from "../../util/auth";
 export default function Header() {
   return (
     <nav className=" bg-indigo-950 p-5">
-      <Link to="/">
-        <h1>DocSwap</h1>
-      </Link>
       {signedIn() ? (
         <>
-          <Link
-            to="/profile"
-            className="relative float-right bottom-12 right-5"
-          >
-            Profile
-          </Link>
+          <Link to="/marketplace">
+            <h1>DocSwap</h1>
+          </Link>{" "}
           <button
             onClick={signOut}
-            className="relative float-right bottom-12 right-5"
+            className="relative float-right bottom-12 right-5 "
           >
             Logout
           </button>
+          <Link
+            to="/profile"
+            className="relative float-right bottom-12 right-5 mr-5"
+          >
+            <button>Profile</button>
+          </Link>
         </>
       ) : (
         <>
+          <Link to="/">
+            <h1>DocSwap</h1>
+          </Link>
           <Link to="/signup" className="relative float-right bottom-12">
             <button>Sign Up</button>
           </Link>
