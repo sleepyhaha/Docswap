ff8d080596b6d563ccc71391ec35fa69fc0cdfcc
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-11-02 17:12:00 +1100

    added a success page

---
+++ client/src/components/success/index.jsx
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div>
+      <p>Whatever this was it was successful! </p>
+      <img src="../../../public/thumbs-thumbs-up-kid.gif" alt="" />
+    </div>
+  );
+}
