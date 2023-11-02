6052cb63c072f6fb2fa1a186754592745c89e966
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-11-02 17:13:48 +1100

    added a success page

--- client/src/components/success/index.jsx
+++ client/src/components/success/index.jsx
@@ -1,8 +1,14 @@
 export default function Success() {
   return (
     <div>
-      <p>Whatever this was it was successful! </p>
-      <img src="../../../public/thumbs-thumbs-up-kid.gif" alt="" />
+      <p className="text-5xl relative left-24">
+        Whatever this was it was successful!{" "}
+      </p>
+      <img
+        src="../../../public/thumbs-thumbs-up-kid.gif"
+        alt=""
+        className="m-auto mt-12"
+      />
     </div>
   );
 }
