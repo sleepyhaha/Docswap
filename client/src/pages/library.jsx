ebbb2cc57e39c4010e7dc9e03c48868916e0ba0a
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-11-02 17:15:47 +1100

    added library view

--- client/src/pages/library.jsx
+++ client/src/pages/library.jsx
@@ -2,12 +2,45 @@ import { Link } from "react-router-dom";
 
 export default function Library() {
   return (
-    <div className="h-2/3">
+    <div className="h-full">
       <section className="flex justify-center w-full">
         <h1>Library</h1>
       </section>
-      <div className="h-full columns-3 w-4/5 flex">
-        <section className="w-72 h-96 bg-stone-300">
+      <div className=" w-4/5 m-auto flex flex-wrap justify-evenly">
+        <section className="w-72 h-96 bg-stone-600 mt-5">
+          <title>Doc title</title>
+          <div className="columns-2">
+            <img src="/" alt="seller profile picture" />
+            <p>description here</p>
+            <p>price here</p>
+          </div>
+          <Link to="/document">
+            <button>View</button>
+          </Link>
+        </section>
+        <section className="w-72 h-96 bg-stone-600 mt-5">
+          <title>Doc title</title>
+          <div className="columns-2">
+            <img src="/" alt="seller profile picture" />
+            <p>description here</p>
+            <p>price here</p>
+          </div>
+          <Link to="/document">
+            <button>View</button>
+          </Link>
+        </section>
+        <section className="w-72 h-96 bg-stone-600 mt-5">
+          <title>Doc title</title>
+          <div className="columns-2">
+            <img src="/" alt="seller profile picture" />
+            <p>description here</p>
+            <p>price here</p>
+          </div>
+          <Link to="/document">
+            <button>View</button>
+          </Link>
+        </section>
+        <section className="w-72 h-96 bg-stone-600 mt-5">
           <title>Doc title</title>
           <div className="columns-2">
             <img src="/" alt="seller profile picture" />
